c0021153efe5b0d7100984da4096c5e66bb5af42
aditya pathak <pathakaditya881@gmail.com>
2025-03-05 04:07:07 +0530

    updated

--- first-project/src/App.jsx
+++ first-project/src/App.jsx
@@ -2,7 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import btnModule from "./Button.module.css" 
+import { Col, Container, Row,Button } from 'react-bootstrap'
+//import btnModule from "./Button.module.css" 
 //import Header from './Header.jsx'
 //import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 //import {faWhatsapp} from '@fortawesome/free-brands-svg-icons'
@@ -14,14 +15,39 @@ function App() {
 
 
   return (
-    <>
-    <div className="App">
-      <button className={btnModule.error}>error</button>
-      <button className={btnModule.warning}>warning</button>
+    <div className="main">
+      {/* <Header/> */}
+      <div className="container">hello</div>
+      <Container fluid>
+        <Container>
+          <Row>
+            <Col>
+              <h1>our courses</h1>
+            </Col>
+          </Row>
+          <Row>
+            <col lg="3" md="6">
+              <Card style={{ width: "18rem" }}>
+                <Card.Img variant="top" src="holder.js/100px180" />
+                <Card.Body>
+                  <Card.Title>Card Title</Card.Title>
+                  <Card.Text>
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </Card.Text>
+                  <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+              </Card>
+            </col>
+            <col lg="3" md="6"></col>
+            <col lg="3" md="6"></col>
+            <col lg="3" md="6"></col>
+          </Row>
+        </Container>
+      </Container>
+      {/* <Footer/> */}
     </div>
-
-    </>
-  )
+  );
   
   
 
